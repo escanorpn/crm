6d24925710da20cdb3b7abf557ef1dbe176b8b3c
escanorpn <94233672+escanorpn@users.noreply.github.com>
2023-09-10 20:30:19 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+// Handle GET request
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $VERIFY_TOKEN = 'abcd';
+    $mode = $_GET['hub_mode'];
+    $challenge = $_GET['hub_challenge'];
+    $verifyToken = $_GET['hub_verify_token'];
+
+    if ($mode === 'subscribe' && $verifyToken === $VERIFY_TOKEN) {
+        echo $challenge;
+        http_response_code(200);
+        exit();
+    } else {
+        http_response_code(403);
+        echo "Verification failed";
+        exit();
+    }
+}
+
+// Handle POST request
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Implement your POST request handling logic here
+    // Example: $data = json_decode(file_get_contents('php://input'), true);
+    // Implement your response logic as needed
+    http_response_code(200);
+    echo "POST request received";
+    exit();
+}
+
+// Handle unsupported request methods
+http_response_code(405);
+echo "Method Not Allowed";
+?>
